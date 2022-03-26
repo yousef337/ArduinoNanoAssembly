3cf5c1a3124cbd9680bd6b63182749d6824355ba
Yousef <Yousef.a.altaher@gmail.com>
2022-03-26 21:48:22 +0000

    Add lab 4

--- CSLabs/Lab4/main.s
+++ CSLabs/Lab4/main.s
@@ -12,15 +12,19 @@ turnOn :
     ldi r17, 0xf
     out DDRB, r17
     out PORTB, r17
-    
+
+    ldi r23, 1
+    ldi r22, 10
+    rjmp loopC
+
+mainloop: rjmp mainloop
+
+turnRecieved:
+
     ldi r17, 0x78
     out PINC, r17
     out PORTC, r17
 
-    ldi r20, 255
-    rjmp loopB
-
-turnRecieved:
     ; Although can be intialized directly, it is just to keep them as a reference to indicate change
     ldi r16,0
     ldi r18,0
@@ -32,7 +36,30 @@ turnRecieved:
     out DDRB, r16
     out PORTB, r16
 
-mainloop: rjmp mainloop
+    ldi r23, 0
+    ldi r22, 10
+    rjmp loopC
+
+onTrue:
+    rjmp turnRecieved
+
+onTrueB:
+    rjmp turnOn
+
+    
+loopC:
+    nop
+
+    ldi r20, 255
+
+    dec r22
+
+    cpi r22, 0
+    brne loopB
+
+    cpi r23, 0
+        brne onTrue
+    rjmp onTrueB    
 
 loopB:
     nop
@@ -42,8 +69,9 @@ loopB:
     dec r20
 
     cpi r20, 0
-    brne turnRecieved
-    rjmp loopB
+    brne loopA
+
+    rjmp loopC
 
 loopA:
     nop
